0e19fc0db29368e91e0c2cd77f36972ea077694a
Ingkharut R <kharutta04@gmail.com>
2026-03-08 22:03:44 +0700

    test make appointment

--- playwright_assignment_3_4/page-objects/loginPage.js
+++ playwright_assignment_3_4/page-objects/loginPage.js
@@ -1,18 +1,16 @@
 export class LoginPage {
   constructor(page) {
     this.page = page
-    this.username = process.env.USERNAME || ''
-		this.password = process.env.PASSWORD || ''
 		this.url = process.env.URL || ''
   }
 
-  async login() {
+  async login(username, password) {
     await this.page.goto(this.url);
     await this.page.getByRole("link", { name: "Make Appointment" }).click();
     await this.page.getByLabel("Username").click();
-    await this.page.getByLabel("Username").fill(this.username);
+    await this.page.getByLabel("Username").fill(username);
     await this.page.getByLabel("Password").click();
-    await this.page.getByLabel("Password").fill(this.password);
+    await this.page.getByLabel("Password").fill(password);
     await this.page.getByRole("button", { name: "Login" }).click();
   }
 }
